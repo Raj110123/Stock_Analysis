5fdf3577037f88ce396977c113ea6c8162c80ad0
Raj110123 <rajrizvi734@gmail.com>
2025-09-06 18:44:10 +0530

    homepage react

---
+++ frontend-react/src/components/Main.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Button from './Button'
+
+const Main = () => {
+  return (
+    <div className='container'>
+    <div className='pt-5 text-center bg-light-dark'>
+        <h1 className='text-light'>Stock prediction portal</h1>
+        <p className='text-light lead'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Soluta id recusandae ipsam fuga voluptas a alias nulla eum neque? Repudiandae quae totam laboriosam repellendus cupiditate! Temporibus, labore nihil. Necessitatibus, ut.</p>
+
+        <Button class='btn btn-outline-info' text='Logon'/>
+    </div>
+    </div>
+  )
+}
+
+export default Main
